53f2cfcaf03d3126f934696c77f548a68fdf348c
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2026-06-03 15:46:33 +0400

    feat: init

---
+++ src/main/java/com/github/mohrezal/springidentity/SpringIdentityApplication.java
@@ -0,0 +1,13 @@
+package com.github.mohrezal.springidentity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIdentityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringIdentityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/mohrezal/springidentity/SpringIdentityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mohrezal.springidentity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIdentityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
