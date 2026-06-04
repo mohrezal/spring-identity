52ebf6fa10c237c4ceeb5e8740e4e2e3b8418993
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2026-06-04 14:08:34 +0400

    feat: add pre-commit hook

--- src/main/java/com/github/mohrezal/identity/SpringIdentityApplication.java
+++ src/main/java/com/github/mohrezal/identity/SpringIdentityApplication.java
@@ -11,5 +11,4 @@ public class SpringIdentityApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringIdentityApplication.class, args);
     }
-
 }
--- src/main/java/com/github/mohrezal/identity/config/ApplicationProperties.java
+++ src/main/java/com/github/mohrezal/identity/config/ApplicationProperties.java
@@ -3,16 +3,13 @@ package com.github.mohrezal.identity.config;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import java.time.Duration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
-import java.time.Duration;
-
 @Validated
 @ConfigurationProperties(prefix = "app")
-public record ApplicationProperties(
-        @Valid Security security
-) {
+public record ApplicationProperties(@Valid Security security) {
 
     @Validated
     public record Security(
@@ -20,6 +17,5 @@ public record ApplicationProperties(
             @NotNull Duration accessTokenTtl,
             @NotNull Duration refreshTokenTtl,
             @NotNull Duration verificationTokenTtl,
-            @NotNull Duration passwordResetTokenTtl
-            ){}
+            @NotNull Duration passwordResetTokenTtl) {}
 }
